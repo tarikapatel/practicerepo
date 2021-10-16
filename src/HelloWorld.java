fbe71bacc9c58765b8874ba16dd47d853fa78a98
tarikapatel <tarikapatel23@gmail.com>
2021-10-16 17:27:50 +0100

    practicetest

---
+++ src/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Tamina");
+        System.out.println(16);
+    }
+
+}
---
+++ src/PersonalDetails.java
@@ -0,0 +1,11 @@
+public class PersonalDetails
+{
+    public static void main (String Args[])
+    {
+    System.out.println("My Name is Tamina.");
+    System.out.println("My address is:  12 xyz Street, HA3 9PT.");
+    System.out.println("Salary" + " " + "=" + " " + "£66,000");
+    System.out.println("12th Grade % is " + " " +  76 );
+    System.out.println("Gender: Female");
+    }
+}
